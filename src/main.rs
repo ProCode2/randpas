91fb36ad19a37d0d2ff94b4e8ce9dafc8a13dc79
ProCode2 <bpro249@gmail.com>
2020-09-17 05:44:22 -0700

    formatted the code

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
-use std::env;
 use rand::{thread_rng, Rng};
+use std::env;
 
 mod parser;
 use crate::parser::*;
 
-
-pub struct Specifications{
+pub struct Specifications {
     generate: bool,
     password_len: u32,
     charset: Vec<u8>,
 }
 
-impl Specifications{
-    fn new() -> Specifications{
-        return Specifications{
-            generate : false,
+impl Specifications {
+    fn new() -> Specifications {
+        return Specifications {
+            generate: false,
             password_len: 30,
             charset: Vec::new(),
         };
@@ -25,17 +24,16 @@ fn main() {
     let mut rng = thread_rng();
     let mut specs = Specifications::new();
     let arguments: Vec<String> = env::args().collect();
-    parser(&arguments , &mut specs);
+    parser(&arguments, &mut specs);
 
     if specs.generate == true {
-
-       let password: String = (0..specs.password_len)
-            .map(|_|{
-            let index: usize= rng.gen_range(0 , specs.charset.len());
-            specs.charset[index] as char
-                            })
+        let password: String = (0..specs.password_len)
+            .map(|_| {
+                let index: usize = rng.gen_range(0, specs.charset.len());
+                specs.charset[index] as char
+            })
             .collect();
 
-        println!("{}" , password);
+        println!("{}", password);
     }
 }
